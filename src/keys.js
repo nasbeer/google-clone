7223a6dfbaba64ed3a4475254c8a859cb470ed84
Nasbeer Ahammed <5653844+nazbeer@users.noreply.github.com>
2021-12-20 16:14:32 +0400

    Issue fix

--- src/keys.js
+++ src/keys.js
@@ -1,6 +1,6 @@
-export const API_KEY = '    AIzaSyD5vySK8n91fuGXT00gN3jNbEtDbKi6wv0    '; // https://developers.google.com/custom-search/v1/using_rest
+export const API_KEY = '    YOUR_API_KEY    '; // https://developers.google.com/custom-search/v1/using_rest
 
-export const CONTEXT_KEY = 'a2a1ea38a321b40db'; // https://console.developers.google.com
-export const auth = '7a2348b66ae00f41c7b8c46745b63a6162bb78b7bc332caa689eaba7bcc921cf'; // https://abhi-api.jubna.me/api/getNewsFeed?start_date=2021-08-23&end_date=2021-10-24%27,
+export const CONTEXT_KEY = 'YOUR_CONTEXT_KEY'; // https://console.developers.google.com
+export const auth = 'YOUR_API_KEY'; // https://abhi-api.jubna.me/api/getNewsFeed?start_date=2021-08-23&end_date=2021-10-24%27,
 
-export const W_KEY = 'be5ab6ae194ebd60a1a13a6230b214cf';// https://api.openweathermap.org/data/2.5/weather?q=dubai&appid=be5ab6ae194ebd60a1a13a6230b214cf
+export const W_KEY = 'YOUR_API_KEY';// https://api.openweathermap.org/data/2.5/weather?q=dubai&appid=be5ab6ae194ebd60a1a13a6230b214cf
